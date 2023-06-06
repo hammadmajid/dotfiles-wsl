b15e51165d53182b94b73f7ba3347403440d3cd2
hammadmajid <hm.hammadmajid@gmail.com>
2023-06-06 22:40:05 +0500

    remove gitui alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,9 +14,6 @@ alias py="python3"
 # set cl to clear
 alias cl="clear -x"
 
-# use catppuccin mocha theme for gitui
-alias gitui="gitui -t mocha.ron"
-
 # alias ls to exa
 alias ls="exa --all --classify --icons --group-directories-first"
 
